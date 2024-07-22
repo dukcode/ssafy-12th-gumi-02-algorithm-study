b6d4f60f26682b55bc729b5f3d79ee1643293877
이성욱 <hohnmb@naver.com>
2024-07-22 22:59:57 +0900

    hohnmb: 백준 입출력과 사칙연선 1~7단계

---
+++ hohnmb/backjoon1001.py
@@ -0,0 +1,4 @@
+#두 수를 입력받고 뺄셈을 한 결과 출력
+A, B = input().split()
+
+print(A-B)
---
+++ hohnmb/backjoon10869.py
@@ -0,0 +1,8 @@
+#모든 연산 문제(사칙연산)
+A, B = input().split()
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)
