deb6ea6ef00394d9f95566ac7b10570afb324a6b
서유민 <senna077@gmail.com>
2024-07-24 21:24:50 +0900

    senna: 백준 10807 문제

--- sennaseo/beakjoon/10952.py
+++ sennaseo/beakjoon/10952.py
@@ -3,3 +3,6 @@ while True:
     if A == 0 and B == 0:
         break
     print(A+B)
+# while 문을 잘 몰라서 계속 for문과 if문을 돌렸다
+# While True 다음의 식이 계속 True일때 계속된다는 뜻
+# if 조건문을 완성하기까지 A, B = map(int, input().split())은 계속 돌아감.
