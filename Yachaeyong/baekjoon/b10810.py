4a18bd94209af96de2146c576ed0cb221c9447b3
채용수 <chaeyong0320@naver.com>
2024-07-25 22:02:37 +0900

    Yachaeyong: 백준 10810 풀이

--- Yachaeyong/baekjoon/b10810.py
+++ Yachaeyong/baekjoon/b10810.py
@@ -1,3 +1,13 @@
-#공 넣기
+# 10810 공 넣기
 
 N, M = map(int, input().split())
+
+box = [0] * N #빈 박스 생성
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for x in range(i, j+1): #i부터 j번 박스까지
+        box[x-1] = k #박스 번호는 1부터 시작인데 리스트 인덱스는 0부터 시작이므로 -1해줌
+
+for y in range(N):
+    print(box[y], end=' ')
