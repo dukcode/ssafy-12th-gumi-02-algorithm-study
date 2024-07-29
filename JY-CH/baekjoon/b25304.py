c1c376fec9d22199994fc5212d7a546466d97506
최 재 영 <horizontal4@naver.com>
2024-07-29 17:57:36 +0900

    25304 푸는중, swea 풀이 추가

---
+++ JY-CH/baekjoon/b25304.py
@@ -0,0 +1,11 @@
+# 영수증
+
+pay_all = int(input())
+var = int(input())
+
+for i in range(var):
+    money, cnt = map(int, input().split())
+    # cost = []
+    # cost[i] = (money[i] * cnt[i])
+    print(money)
+    
