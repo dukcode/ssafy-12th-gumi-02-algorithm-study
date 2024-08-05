f8a46bd7e8dc83335a0073d9a28f0ad946110189
최 재 영 <horizontal4@naver.com>
2024-08-05 17:53:10 +0900

    날씨 무엇?

---
+++ JY-CH/baekjoon/b2563.py
@@ -0,0 +1,13 @@
+# 색종이
+
+arr = [[0] * 100 for _ in range(100)]
+
+num = int(input())
+
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+
+
+cnt = 0
+if 
