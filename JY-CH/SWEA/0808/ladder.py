084330ca3f01d9b2dd1682fd392b22508c772426
최 재 영 <horizontal4@naver.com>
2024-08-09 17:30:16 +0900

    금요일!

--- JY-CH/SWEA/0808/ladder.py
+++ JY-CH/SWEA/0808/ladder.py
@@ -1,21 +1,36 @@
 # 사~다~리~잉
 
-t = int(input())
+def solve(r, c):
 
-for tc in range(1, t+1):
-    arr = [[list(map(int, input().split()))] for _ in range(100)]
-
-    # 어차피 탈게 사다리면 위로 갈 필요가 없는데
-    # 빼보자!
-    mr = [0, 1, 0]
-    mc = [1, 0, -1]
     way = 0
-    row, col = 0, 0
+    mr = [1, 0, 0]
+    mc = [0, -1, 1]
+    while r < 99:
+        r += mr[way]
+        c += mc[way]
 
-    for row in range(100):
-        if arr[row][col] == 0:
-            continue
+        if way == 0:
+            if c > 0 and arr[r][c-1] == 1:
+                way = 1
+            elif c < 99 and arr[r][c+1] == 1:
+                way = 2
         else:
-            row += mr[way]
-            col += mc[way]
+            if r < 99 and arr[r+1][c] == 1:
+                way = 0
+
+    if arr[r][c] == 2:
+        return True
+    else:
+        return False
+
+for _ in range(10):
+    tc = input()
+    arr = [list(map(int, input().split())) for _ in range(100)]
+
+    for i in range(100):
+        if arr[0][i] == 1:
+            if solve(0, i) == True:
+                print(f'#{tc} {i}')
+                break
+
 
