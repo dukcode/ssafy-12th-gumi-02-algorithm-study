521dece8edef60c54040339f16521fd356cb556e
최 재 영 <horizontal4@naver.com>
2024-08-16 07:59:23 +0900

    JY-CH: 백준 1158 풀이

--- JY-CH/baekjoon/b1158.py
+++ JY-CH/baekjoon/b1158.py
@@ -1,11 +1,16 @@
 # 요세푸스;;
 
-# N, K = map(int, input().split())
+from collections import deque
 
-live_list = []
-for i in range(1, N+1):
-    live_list.append(i)
+N, K = map(int, input().split())
 
-dead_list = []
+live_list = list(range(1, N + 1))
+death_list = []
+q = deque(live_list)
 
+while q:
+	q.rotate( K * (-1) )
+	H = q.pop()
+	death_list.append(str(H))
 
+print("<", ", ".join(death_list), ">", sep='')
