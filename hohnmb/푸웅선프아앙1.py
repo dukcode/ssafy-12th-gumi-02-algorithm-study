06d02fa3ac4faf66613945673fb91cdedbab86a6
이성욱 <hohnmb@naver.com>
2024-08-17 14:01:44 +0900

    hohnmb swea 풍선팡1 풀이

---
+++ hohnmb/푸웅선프아앙1.py
@@ -0,0 +1,26 @@
+
+di = [0,1,0,-1]
+dj = [1,0,-1,0]
+
+
+T = int(input())
+
+for tc in range(1,T+1):
+    n, m = map(int,input().split())
+    f_powder = [list(map(int,input().split())) for _ in range(n)]
+    max_value = 0
+    for i in range(n):
+        for j in range(m):
+            start = f_powder[i][j]
+            for k in range(4):
+                for l in range(1, f_powder[i][j]+1):
+                    ni = i+di[k]*l
+                    nj = j+dj[k]*l
+                    if 0 <= ni < n and 0 <= nj < m :
+                        start += f_powder[ni][nj]
+            if start > max_value:
+                max_value = start
+
+    print(f'#{tc} {max_value}')
+
+
