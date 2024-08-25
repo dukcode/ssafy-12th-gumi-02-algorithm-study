44c1d2cbcb66805a51d457fd2a544a5fba65a1b8
최 재 영 <horizontal4@naver.com>
2024-08-26 00:09:28 +0900

    JY-CH: SWEA 1219 풀이

--- JY-CH/SWEA/0825/s3143.py
+++ JY-CH/SWEA/0825/s3143.py
@@ -4,16 +4,39 @@ T = int(input())
 for tc in range (1, T+1):
     check, word = map(str, input().split())
 
-    cnt = 0
+    # cnt = 0
+    # idx = 0
+    # while idx <= len(check) - len(word):
+    #     if check[idx:idx+len(word)] == word:
+    #         cnt += 1
+    #         idx += len(word)
+    #     else:
+    #         idx += 1
+    
+    
+    # result = len(check) - (cnt * len(word)) + cnt
+    # print(f'#{tc} {result}')
+
+
+    N = len(check)
+    M = len(word)
+    result = 0
     idx = 0
-    while idx <= len(check) - len(word):
-        if check[idx:idx+len(word)] == word:
-            cnt += 1
-            idx += len(word)
-        else:
-            idx += 1
+    for i in range(idx, N):
+        cnt = 0
+        if idx+i > N:
+            break
+        for j in range(M):
+            if check[idx+i+j] == word[j]:
+                cnt += 1
+
+        if cnt == M:
+            result += 1
+            idx += M
 
 
-    result = len(check) - (cnt * len(word)) + cnt
-    print(f'#{tc} {result}')
-        
+    result2 = len(check) - (result * len(word)) + result
+
+    print(f'#{tc} {result2}')
+    print()
+
