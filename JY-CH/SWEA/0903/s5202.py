0a08eda0b64c1026bd9f7b8f5133e583fb912219
최 재 영 <horizontal4@naver.com>
2024-09-03 22:07:49 +0900

    JY-CH: SWEA 5202 풀이

--- JY-CH/SWEA/0903/s5202.py
+++ JY-CH/SWEA/0903/s5202.py
@@ -3,4 +3,19 @@
 t = int(input())
 for tc in range(1, t+1):
     work_cnt = int(input())
-    start, end = map(int, input().split())
+    lst = []
+    for _ in range(work_cnt):
+        start, end = map(int, input().split())
+        lst.append([start, end])
+        lst.sort(key=lambda x: x[1])
+
+    cnt = 0
+    last_end = 0
+    for start, end in lst:
+        if last_end <= start:
+            cnt += 1
+            last_end = end
+
+    print(f'#{tc} {cnt}')
+
+
