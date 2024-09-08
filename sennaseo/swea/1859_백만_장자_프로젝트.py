b8f3f3889fc0170bf32ad6b6a46629cd35e1fdb3
서유민 <senna077@gmail.com>
2024-09-08 23:17:31 +0900

    senna: swea 백만 장자 프로젝트 문제 풀이

---
+++ sennaseo/swea/1859_백만_장자_프로젝트.py
@@ -0,0 +1,32 @@
+def solve2():
+    max_v = data[-1]
+    money = 0
+    for i in range(N - 1, -1, -1):
+        if data[i] <= max_v:
+            money += max_v - data[i]
+        else:
+            max_v = data[i]
+    return money
+
+
+def solve():
+    money = 0
+    start = 0
+    while start < N:
+        max_idx = start  #
+        for i in range(start, N):
+            if data[i] >= data[max_idx]:
+                max_idx = i
+        # 물건 사서 팔기
+        for i in range(start, max_idx):
+            money += data[max_idx] - data[i]
+        start = max_idx + 1
+    return money
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    data = list(map(int, input().split()))
+    result = solve2()
+    print(f"#{tc} {result}")
