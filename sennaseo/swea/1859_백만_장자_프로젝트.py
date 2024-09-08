975289a291308d5b76aafbd5b65e627828f39c69
서유민 <senna077@gmail.com>
2024-09-08 23:20:33 +0900

    senna: swea 백만 장자 프로젝트

--- sennaseo/swea/1859_백만_장자_프로젝트.py
+++ sennaseo/swea/1859_백만_장자_프로젝트.py
@@ -1,19 +1,8 @@
-def solve2():
-    max_v = data[-1]
-    money = 0
-    for i in range(N - 1, -1, -1):
-        if data[i] <= max_v:
-            money += max_v - data[i]
-        else:
-            max_v = data[i]
-    return money
-
-
 def solve():
     money = 0
     start = 0
     while start < N:
-        max_idx = start  #
+        max_idx = start
         for i in range(start, N):
             if data[i] >= data[max_idx]:
                 max_idx = i
