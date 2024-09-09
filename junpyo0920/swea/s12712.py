74ac76aca3248f7ef5d491a5b40b046997bdd568
June0920 <junpyo0920@gmail.com>
2024-09-10 08:31:06 +0900

    junpyo0920: SWEA 12712 파리퇴치3 문제 풀이

--- junpyo0920/swea/s12712.py
+++ junpyo0920/swea/s12712.py
@@ -1,33 +1,33 @@
+dy = [-1, 0, 1, 0, -1, -1, 1, 1]
+dx = [0, 1, 0, -1, -1, 1, 1, -1]
+
+
+def get_flies(y, x, w):
+    ret = data[y][x]
+    for p in range(1, m):
+        if w == 0:
+            for d in range(4):
+                ny, nx = y + (dy[d] * p), x + (dx[d] * p)
+                if 0 <= ny < n and 0 <= nx < n:
+                    ret += data[ny][nx]
+        elif w == 1:
+            for d in range(4, 8):
+                ny, nx = y + (dy[d] * p), x + (dx[d] * p)
+                if 0 <= ny < n and 0 <= nx < n:
+                    ret += data[ny][nx]
+        else:
+            return -1
+    return ret
+
+
 for tc in range(int(input())):
-    ans = 0
-    N, M = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
+    n, m = map(int, input().split())
+    data = [list(map(int, input().split())) for _ in range(n)]
 
-    def get_score(power, init_y, init_x):
-        score_1 = 0
-        score_2 = 0
-        way_1 = ((y, x) for y in range(-power + 1, power) for x in range(-power + 1, power) if
-                 abs(y) == abs(x))
-        way_2 = ((y, x) for y in range(-power + 1, power) for x in range(-power + 1, power) if
-                 y == 0 or x == 0)
-        
-        for direction_1 in way_1:
-            idx_y = init_y + direction_1[0]
-            idx_x = init_x + direction_1[1]
-            if 0 <= idx_y < N and 0 <= idx_x < N:
-                score_1 += matrix[idx_y][idx_x]
+    ans = -0xFFFFFFFF
+    for y in range(n):
+        for x in range(n):
+            for way in range(2):
+                ans = max(ans, get_flies(y, x, way))
 
-        for direction_2 in way_2:
-            idx_y = init_y + direction_2[0]
-            idx_x = init_x + direction_2[1]
-            if 0 <= idx_y < N and 0 <= idx_x < N:
-                score_2 += matrix[idx_y][idx_x]
-
-        return max(score_1, score_2)
-
-    for y in range(N):
-        for x in range(N):
-            score = get_score(M, y, x)
-            ans = max(ans, score)
-
-    print(f'#{tc + 1} {ans}')
+    print(f"#{tc+1} {ans}")
