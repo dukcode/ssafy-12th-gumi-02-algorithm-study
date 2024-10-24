c5b5bd2b3c48eedf47b76097fbf79cf305c029be
채용수 <chaeyong0320@naver.com>
2024-10-24 22:50:43 +0900

    Yachaeyong: 백준 1946 풀이

--- Yachaeyong/baekjoon/b1946.py
+++ Yachaeyong/baekjoon/b1946.py
@@ -6,13 +6,12 @@ for _ in range(T):
     N = int(input())
     ranking = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-    ranking.sort(key=lambda x: -x[0])
+    ranking.sort()
 
-    cnt = 0
-    for f in range(N):
-        for s in range(f + 1, N):
-            if ranking[f][0] < ranking[s][0] and ranking[f][1] < ranking[s][1]:
-                cnt += 1
-                break
-    ans = N - cnt
-    print(ans)
+    cnt = 1
+    cut_line = 0
+    for i in range(1, N):
+        if ranking[i][1] < ranking[cut_line][1]:
+            cnt += 1
+            cut_line = i
+    print(cnt)
