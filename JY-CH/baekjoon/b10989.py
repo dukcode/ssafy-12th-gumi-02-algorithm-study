ee1e67d7c1a90571a80105d99063db6619d44e6d
최 재 영 <horizontal4@naver.com>
2024-11-22 21:29:19 +0900

    JY-CH: 백준 1978 풀이

--- JY-CH/baekjoon/b10989.py
+++ JY-CH/baekjoon/b10989.py
@@ -1,23 +1,15 @@
 # 수 정렬하기 3
+import sys
+input = sys.stdin.readline
 
-# 입력값을 받자
+data = []
 n = int(input())
-# 카운트 배열 받을거다. 마지막 범위엔 1을 추가하자.
-# 안하면 마지막수가 짤린다.
-# 입력값을 전부 받아야 하니까 마지막 범위에 1을 추가한다.
-count = [0] * (n+1)
-# for문을 통해 입력값 다 받고
-for i in range(1, n+1):
-    num = int(input())
-    # count index에 입력 받은 숫자를 할당, 해당 인덱스에 +1
-    count[num] += 1
-print(count)
+for i in range(n):
+    data.append(int(input()))
+    if len(data) >= 2:
+        while True:
+            for j in range(1, i + 1):
+                if data[j - 1] > data[j]:
+                    data[j - 1], data[j] = data[j], data[j - 1]
 
-# 이러면 count는 인덱스에 해당 값의 갯수당 +1을 할당 받을것.
-# 이제 이걸 꺼내보자
-
-# n개까지 반복할거니까 +1
-for i in range(n+1):
-    # i를 반복할때 count[i]까지 돌면서
-    for _ in range(count[i]):
-        print(i)
+print(data)
---
+++ JY-CH/baekjoon/b1978.py
@@ -0,0 +1,26 @@
+# 소수 찾기
+
+
+def find(number):
+    if number > 2:
+        for i in range(2, number):
+            if number % i == 0:
+                return False
+        return True
+    elif number == 2:
+        return True
+    else:
+        return False
+            
+
+
+
+
+
+n = int(input())
+data = list(map(int, input().split()))
+cnt = 0
+for i in range(n):
+    if find(data[i]):
+        cnt += 1
+print(cnt)
