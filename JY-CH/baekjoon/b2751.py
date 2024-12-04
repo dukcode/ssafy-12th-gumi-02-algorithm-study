83352d159c05e51ebbf599c610435229b201d7c9
최 재 영 <horizontal4@naver.com>
2024-12-04 22:14:16 +0900

    JY-CH: 백준 2751 풀이2

--- JY-CH/baekjoon/b2751.py
+++ JY-CH/baekjoon/b2751.py
@@ -3,21 +3,31 @@ import sys
 input = sys.stdin.readline
 
 
-
 n = int(input())
-data = [0] * 1000001
-minus_data = [0] * 1000000
+data = []
 for _ in range(n):
-    x = int(input())
-    if x >= 0:
-        data[x] += 1
-    else:
-        minus_data[-x] += 1
+    data.append(int(input()))
 
-for i in range(len(minus_data) - 1, -1 , -1):
-    if minus_data[i]:
-        print(-i)
+data.sort()
 
-for j in range(len(data)):
-    if data[j]:
-        print(j)
+for i in data:
+    print(i)
+
+
+# n = int(input())
+# data = [0] * 1000001
+# minus_data = [0] * 1000000
+# for _ in range(n):
+#     x = int(input())
+#     if x >= 0:
+#         data[x] += 1
+#     else:
+#         minus_data[-x] += 1
+
+# for i in range(len(minus_data) - 1, -1 , -1):
+#     if minus_data[i]:
+#         print(-i)
+
+# for j in range(len(data)):
+#     if data[j]:
+#         print(j)
