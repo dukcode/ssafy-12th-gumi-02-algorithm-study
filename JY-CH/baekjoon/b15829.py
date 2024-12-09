4f5103b057b69c911d3a3fd7a92e054ec8bf3b71
(최재영) <(horizontal4@naver.com)>
2024-12-09 19:37:41 +0900

    JY-CH: 백준 15829 풀이

---
+++ JY-CH/baekjoon/b15829.py
@@ -0,0 +1,35 @@
+# Hashing
+import sys
+input = sys.stdin.readline
+
+
+# 1번 풀이
+# dict = {
+#     'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
+#     'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
+#     'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
+#     'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
+#     'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
+#     'z': 26
+# }
+
+
+# n = int(input())
+# data = list(input())
+
+# result = 0
+# m = 1234567891
+# for i in range(n):
+#     result += int(dict[data[i]] * ((31 ** i) % m)
+# print(result)
+
+
+n = int(input())
+data = list(input())
+
+result = 0
+m = 1234567891
+for i in range(n):
+    result += (ord(data[i]) - 96) * ((31 ** i) % m)
+    result %= m
+print(result)
