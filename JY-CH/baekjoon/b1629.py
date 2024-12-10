6d44921f0242465f89fb0161e3c11f6710280dbe
(최재영) <(horizontal4@naver.com)>
2024-12-10 21:47:35 +0900

    JY-CH: 백준 1629 풀이

--- JY-CH/baekjoon/b1629.py
+++ JY-CH/baekjoon/b1629.py
@@ -1,5 +1,9 @@
 # 곱셈
 
+# 일일이 처리하면 시간 초과남
+# 재귀를 이용, b가 0,1,2일때만 계산
+# 미친 문제;;
+
 def result(a, b, c):
     if b == 0:
         return 1
@@ -9,6 +13,6 @@ def result(a, b, c):
     else:  
         return (a * result(a, b - 1, c)) % c
 
-# 입력 처리
+
 a, b, c = map(int, input().split())
 print(result(a, b, c))
