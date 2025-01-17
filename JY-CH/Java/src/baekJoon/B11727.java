5231f1666625b008b86db17af965abe0fce6d3bc
최재영 <horizontal4@naver.com>
2025-01-17 09:29:06 +0900

    JY-CH: 백준 Java 11727 풀이

---
+++ JY-CH/Java/src/baekJoon/B11727.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class B11727 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int x = Integer.parseInt(input);
+        if (x == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        if (x == 2) {
+            System.out.println(3);
+            return;
+        }
+
+        int[] dp;
+        dp = new int[x+1];
+
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i < x + 1; i++) {
+            dp[i] = ((dp[i-2] * 2)  + dp[i-1]) % 10007;
+        }
+
+
+        System.out.println(dp[x]);
+        scanner.close();
+    }
+
+}
